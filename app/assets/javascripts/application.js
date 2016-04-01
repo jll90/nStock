4faaed0b86c221f2e725c50890459145f817f721
jesus <jesus.luongo.lizana@gmail.com>
2016-03-31 22:42:37 -0300

    missing alignment '

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,30 @@
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
+
+$(document).ready(function(){
+	
+	$basePrice = $("#product_base_price");
+	$afterTaxPrice = $("#product_after_tax_price");
+	$wholesalePrice = $("#product_wholesale_price");
+	$retailPrice = $("#product_retail_price");
+
+
+	$basePrice.on('keyup', function(){
+		var basePrice, afterTaxPrice, wholesalePrice, retailPrice;
+		basePrice = $(this).val();
+		if ( isNaN(parseInt(basePrice)) && (basePrice !== '') ){
+			alert("Ingresa un numero valido");
+		}  else {
+
+			afterTaxPrice = basePrice * 1.19;
+			wholesalePrice = afterTaxPrice * 1.50;
+			retailPrice = afterTaxPrice * 2;
+
+			$afterTaxPrice.val(afterTaxPrice);
+			$wholesalePrice.val(wholesalePrice);
+			$retailPrice.val(retailPrice);
+			
+		}
+	});
+});
