9237df07704e8322801f956d2847aa5eb525a287
jesus <jesus.luongo.lizana@gmail.com>
2016-04-10 23:17:02 -0300

    flash after creating product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,15 +26,11 @@ class ProductsController < ApplicationController
   # POST /products.json
   def create
     @product = Product.new(product_params)
-
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
+    if @product.save
+      flash[:success] = "Producto creado satisfactoriamente"
+      redirect_to products_path
+    else
+      render :new
     end
   end
 
